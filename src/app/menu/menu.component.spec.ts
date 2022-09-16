d7d75022a838fbf5dca867c214ed4aa46ff57576
Francisco Santana <francisco.jsantana3@gmail.com>
2022-09-16 16:12:38 -0300

    Component Test

--- src/app/menu/menu.component.spec.ts
+++ src/app/menu/menu.component.spec.ts
@@ -1,16 +1,55 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { By } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable, of } from 'rxjs';
+import { DishService } from '../services/dish.service';
+import { baseURL } from '../shared/baseurl';
+import { Dish } from '../shared/dish';
+import { DISHES } from '../shared/dishes';
 import { MenuComponent } from './menu.component';
 
 describe('MenuComponent', () => {
-  let component: MenuComponent;
-  let fixture: ComponentFixture<MenuComponent>;
+  let component: MenuComponent | any;
+  let fixture: ComponentFixture<MenuComponent> | any;
 
   beforeEach(async () => {
+    const dishServiceStub = {
+      getDishes: function (): Observable<Dish[]> {
+        return of(DISHES);
+      },
+    };
+
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      imports: [
+        FlexLayoutModule,
+        BrowserAnimationsModule,
+        RouterTestingModule.withRoutes([
+          {
+            path: 'menu',
+            component: MenuComponent,
+          },
+        ]),
+        MatProgressSpinnerModule,
+      ],
+      declarations: [MenuComponent],
+      providers: [
+        {
+          provide: DishService,
+          useValue: dishServiceStub,
+        },
+        {
+          provide: 'BaseURL',
+          useValue: baseURL,
+        },
+      ],
+    }).compileComponents();
+
+    const dishservice = TestBed.get(DishService);
 
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
@@ -19,5 +58,22 @@ describe('MenuComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('dishes items should be 4', () => {
+    expect(component?.dishes?.length).toBe(4);
+    expect(component?.dishes[1]?.name).toBe('Zucchipakoda');
+    expect(component?.dishes[3]?.featured).toBeFalsy();
+  });
+
+  it('should use dishes in the template', () => {
+    fixture.detectChanges();
+
+    let de: DebugElement;
+    let el: HTMLElement;
+    de = fixture.debugElement.query(By.css('h1'));
+    el = de.nativeElement;
+
+    expect(el.textContent).toContain(DISHES[0].name.toUpperCase());
   });
 });
